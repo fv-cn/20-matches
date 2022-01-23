d13451ada64faaa96f98821d423cf7cac4cb6652
fadcon <fad.con0909@gmail.com>
2022-01-23 21:32:40 +0400

    solution

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,45 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static int ComputerMove(int matches) {
+        return 4 - matches;
+    }
 
+    public static int PlayerMove() {
+        Scanner sc = new Scanner(System.in);
+        return sc.nextInt();
+    }
+
+    public static void main(String[] args) {
+        int numOfMatches = 20;
+        int playerMatches = 1;
+        int computerMatches;
+
+        System.out.println("На столе осталось " + numOfMatches + " спичек.");
+
+        while(true) {
+            computerMatches = ComputerMove(playerMatches);
+            numOfMatches -= computerMatches;
+            System.out.println(" - Количество выбранных компьютером спичек = " + computerMatches);
+            if (numOfMatches == 1) break;
+            System.out.println("На столе осталось " + numOfMatches + " спичек.");
+
+
+            while(true) {
+                System.out.println(" - Ход игрока. Введите количество спичек: ");
+                playerMatches = PlayerMove();
+
+                if (playerMatches > 3 || playerMatches < 1) System.out.println("Некорректное количество спичек!");
+                else break;
+            }
+
+            numOfMatches -= playerMatches;
+            System.out.println("На столе осталось " + numOfMatches + " спичек.");
+        }
+
+        System.out.println("Для игрока осталась последняя спичка. Игрок проиграл!");
     }
 }
